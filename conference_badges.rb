dde9ac1e4e8d9f60a8d294cfa32cc5feabdc152d
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-24 21:33:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def batch_badge_creator(array)
+  badge_array = []
+  array.each{|x| badge_array << "Hello, my name is #{x}."}
+  return badge_array
+end 
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+i = 0 
+
+def assign_rooms(name)
+  if i < 7 
+    return "Hello, #{array[i]}! You'll be assigned to room #{i+1}!"
+    i += 1 
+  end 
+
+end 
+
+def printer 
+  print batch_badge_creator(array)
+  print assign_rooms(array)
+end 
+  
