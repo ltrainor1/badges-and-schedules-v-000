4b397620985553743c8d4ab785b54e01d4959b24
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-24 21:34:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,13 +11,14 @@ def badge_maker(name)
 end 
 
 i = 0 
-
+room_array = []
 def assign_rooms(name)
   if i < 7 
-    return "Hello, #{array[i]}! You'll be assigned to room #{i+1}!"
+    room_array << "Hello, #{array[i]}! You'll be assigned to room #{i+1}!"
     i += 1 
   end 
-
+  j = 0 
+  while j < room_array.validates_length_of
 end 
 
 def printer 
